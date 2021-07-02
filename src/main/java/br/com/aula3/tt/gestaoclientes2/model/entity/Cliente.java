4cd5acaa27e1733c564ec37cb41eb63703859b51
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 17:21:51 -0300

    criação da entidade cliente

---
+++ src/main/java/br/com/aula3/tt/gestaoclientes2/model/entity/Cliente.java
@@ -0,0 +1,47 @@
+package br.com.aula3.tt.gestaoclientes2.model.entity;
+
+import java.util.List;
+
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private List<Pedido> pedidos;
+
+    public Cliente() {}
+
+    public Cliente(Long id, String nome, String cpf, String email, String telefone, List<Pedido> pedidos) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.pedidos = pedidos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+}
