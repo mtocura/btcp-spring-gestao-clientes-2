8dc992d50426aea02e689907939a93b300cc7242
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 17:13:50 -0300

    Inicialização do projeto spring boot

---
+++ src/main/java/br/com/aula3/tt/gestaoclientes2/GestaoClientes2Application.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tt.gestaoclientes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoClientes2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoClientes2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/aula3/tt/gestaoclientes2/GestaoClientes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tt.gestaoclientes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoClientes2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
